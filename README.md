844deb24cc41f1ff6b1f55107224e96d3eb958d8
MikaAK <mika@kalathil.me>
2021-08-16 15:25:34 -0700

    chore: add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Trading Tools
+
+
+### MonoRepo for:
+- https://github.com/MikaAK/trade_predictor
+- https://github.com/MikaAK/ibkr-trade-log
+- https://github.com/MikaAK/elon_bot
--- stock_chart_visualizer/download_charts.exs
+++ stock_chart_visualizer/download_charts.exs
@@ -1,4 +1,10 @@
-stock_url = fn symbol -> "http://localhost:4000/#{symbol}/2020/07/10" end
+stock_url = fn
+  symbol, <<year::byte-size(4), "-", month::byte-size(2), "-", day:byte-size(2)>> ->
+    "http://localhost:4000/#{symbol}/#{year}/#{month}/#{day}"
+end
+
+generation_symbols = ["SPY"]
+generation_dates = {Date.new(1995, 1, 1), Date.utc_today()}
 
 
 Application.put_env(:hound, :driver, "chrome_driver")
@@ -12,8 +18,9 @@ alias Hound.Helpers.{Navigation, Session}
 
 session_id = Hound.start_session(browser: "chrome_headless")
 
-Process.sleep(10000)
 Session.change_session_to(session_id)
+
 Navigation.navigate_to(stock_url.("SPY"))
 
-Process.sleep(10000)
+
+
